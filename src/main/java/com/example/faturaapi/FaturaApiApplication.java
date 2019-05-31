979ae9b3833ff03549199bc24f33f093cabbac68
Felipecan <felipe.cunha@eng.ci.ufpb.br>
2019-05-31 17:32:15 -0300

    add project

---
+++ src/main/java/com/example/faturaapi/FaturaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.faturaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaturaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaturaApiApplication.class, args);
+	}
+
+}
